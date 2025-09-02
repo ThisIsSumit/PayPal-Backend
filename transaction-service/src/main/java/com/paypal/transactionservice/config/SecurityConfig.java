a278c6267a3402a5331ca06a2bee86f6ba0a02e6
ThisIsSumit <sumitkumar453827@gmail.com>
2025-09-02 17:11:25 +0530

    kafka and zookeeper added for in project pom.xml though docker-compose.yml and mapper configuration added in transaction

--- transaction-service/src/main/java/com/paypal/transactionservice/config/SecurityConfig.java
+++ transaction-service/src/main/java/com/paypal/transactionservice/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.paypal.transactionservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,7 +15,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/transactions/**").permitAll()
                         .anyRequest().authenticated()
