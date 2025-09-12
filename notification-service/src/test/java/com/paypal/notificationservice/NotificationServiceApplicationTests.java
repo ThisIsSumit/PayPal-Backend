f32dc4ce599345dd70bf573d8bfa01bd0b6a4604
ThisIsSumit <sumitkumar453827@gmail.com>
2025-09-12 17:22:39 +0530

    notification-service added

---
+++ notification-service/src/test/java/com/paypal/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paypal.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
